51b063ed5aaadd28d124386aa86198afdd1a12ac
冰森 <dingheng.hdh@alibaba-inc.com>
2021-08-21 02:27:15 +0800

    todolist: 使用 ejs 模板

--- todo-list/src/controller/home.ts
+++ todo-list/src/controller/home.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Provide, Inject } from '@midwayjs/decorator';
 import { Context } from 'egg';  // egg 中 ctx 的定义
 // import * as DB from '../service/fileDB';
+import { renderFile } from 'ejs';
+import { join } from 'path';
 
 @Provide()
 @Controller('/')
@@ -17,23 +19,7 @@ export class HomeController {
     // 告诉浏览器，当前返回的是 HTML 页面（而不是纯文本）
     this.ctx.type = 'html';
     const todoList = this.db.list();
-
-    return `
-      动态渲染
-      <form action="/api/todo" method="POST">
-        <input name="text" /> <button>确定</button>
-      </form>
-
-      <ul>
-        ${todoList.map((item) => `
-          <li>
-            <form action="/api/todo/delete" method="get">
-              ${item}
-              <input name="text" type="hidden" value=${item} />
-              <button>删除</button>
-            </form>
-          </li>
-        `).join('')}
-      </ul>`;
+    const html = await renderFile(join(__dirname, '../app/view/home.ejs'), { list: todoList });
+    return html;
   }
 }
